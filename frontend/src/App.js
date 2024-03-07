9f705f3da10201876c773560e1e4ad82296d993b
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-07 15:43:15 -0800

    added home, login, and register UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header';
-import Home from './pages/Home';
-import  Login  from './pages/Login';
-import Register from './pages/Register';
-
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
     <>
       <Router>
         <div className="container">
-          <Header/>
+          <Header />
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
