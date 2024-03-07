f998089f4020d15abc5f6168488be065394001e5
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-06 20:41:56 -0800

    added server routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Server....")
+const express = require("express")
+const dotenv = require("dotenv").config()
+const {errorHandler} = require("./middleware/errorMiddleware") 
+const colors = require("colors")
+const PORT = process.env.PORT || 8000
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res) =>{
+    res.status(200).json({message: "Welcome to the ticket manager app"})
+})
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.use("/api/login", require("./routes/userRoutes"))
+
+app.use(errorHandler)
+
+app.listen(PORT, ()=> {
+    console.log(`Server started on port ${PORT}`)
+})
+
