073c123466d2c3ef0a1c022e5d511cdbb80ebea4
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-11 15:41:54 -0700

    added protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Header from "./components/Header"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import NewTicket from "./pages/NewTicket"
+import PrivateRoute from "./components/PrivateRoute"
+
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
@@ -17,6 +20,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
