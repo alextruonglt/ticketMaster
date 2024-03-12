f609434d38989679e0290c3715db35970da0f769
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2024-03-12 16:43:53 -0700

    added group ticket view

--- frontend/src/pages/Tickets.js
+++ frontend/src/pages/Tickets.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { getTickets, reset } from "../features/tickets/ticketSlice"
 import Spinner from "../components/Spinner"
 import BackButton from "../components/BackButton"
-
+import TicketItem from "../components/TicketItem"
 const Tickets = () => {
   const { tickets, isLoading, isSuccess } = useSelector(
     (state) => state.tickets
@@ -26,7 +26,23 @@ const Tickets = () => {
   if (isLoading) {
     return <Spinner />
   }
-  return <div>Tickets</div>
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => {
+          return <TicketItem key={ticket._id} ticket={ticket} />
+        })}
+      </div>
+    </>
+  )
 }
 
 export default Tickets
